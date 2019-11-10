005a0bff6ec2d4bf44cb5a9edea5b4c245bb4035
Khai Nguyen <khaimap2015@gmail.com>
2019-11-10 23:49:59 +0700

    Update socket server (11:49-10/11/2019)

--- face_recognize_app_socket_sever.py
+++ face_recognize_app_socket_sever.py
@@ -95,7 +95,7 @@ def ten_image_average(frames):
                     stackprob[name] = proba
                     track_stack[name] = 1
                     named_frame[name] = frame
-                    print(name, " ", proba)
+                    # print(name, " ", proba)
                 else:
                     stackprob[name] += proba
                     track_stack[name] += 1
@@ -115,11 +115,12 @@ def ten_image_average(frames):
         maxi = max(stackprob.items(), key=operator.itemgetter(1))[0]
         # get the average of range
         max_value = stackprob[maxi] / track_stack[maxi]
-        print(maxi, " ", max_value)
     except:
-        return
-    if(maxi == "unknow"):
         return (maxi, max_value, None)
+
+    if(maxi == "unknow"):
+        return (maxi, max_value, None) 
+
     return (maxi, max_value, named_frame[name])
 
 
@@ -192,6 +193,7 @@ def convert_ByteDataArray_To_Mat(listOfByteArray):
     frameList = []
     for arrayOfByte in listOfByteArray:
         imgdata = decode_String_To_Byte(arrayOfByte)
+        # print(len(imgdata))
         frame = convert_ByteData_To_Mat(imgdata)
         frameList.append(frame)
     return frameList
@@ -243,44 +245,87 @@ def collectStaticData(imageArray):  # path point to folder including dataset
                 # the face
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
-                roi_color = frame[startY:endY, startX:endX]
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              (0, 0, 255), 2)
+                # roi_color = frame[startY:endY, startX:endX]
+                roi_color = frame
                 faceArray.append(roi_color)
                 # cv2.imwrite(image, roi_color) # save image into old file with path (  image variable )
 
     return faceArray
 
 
-def train(clientSocket, clientSocketArrdress, clientSocketPort):
-    studentId = ""
-    arrayOfByte = bytearray()
+def readLineFromSocketStream(clientSocket, clientSocketArrdress, clientSocketPort):
+    signal = ""
     while(True):
-        clientSocket.sendall(b"OK\n")
         try:
-            byteReceived = bytearray(clientSocket.recv(1024))
+            byteReceived = bytearray(clientSocket.recv(1))
         except ConnectionResetError:
             print("Connection reset")
             return
-
+            
         print("Bytes received: ", len(byteReceived))
 
-        if(byteReceived[-1] == 10):
-            byteReceived.pop(-1)
-            signal = byteReceived.decode("utf-8")
-            if(signal == "EXIT"):
-                return
-            studentId = signal
-            print(studentId)
+        if(byteReceived[0] != 10):
+            signal += byteReceived.decode("utf-8")
+            continue
 
+        if(byteReceived[0] == 10):
+            print(signal)
+            break
+    return signal
+
+#Convert image from Mat type to byte type
+def convertMatFrameTypeToByteFrameType(frame):
+    im = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    output = BytesIO()
+    im.save(output, format='JPEG')
+    im_data = output.getvalue()
+    return im_data
+
+def realTimeFaceDetect(clientSocket, clientSocketArrdress, clientSocketPort):
+    arrayOfByte = bytearray()
+    clientSocket.sendall(b"OK\n")
+    while(True):
+        # signal = readLineFromSocketStream(clientSocket, clientSocketArrdress, clientSocketPort)
+        # if(signal == "EXIT"):
+        #     break
         arrayOfByte = handle_Image_Send_From_Client(clientSocket, clientSocketArrdress, clientSocketPort)
         listOfByteArray = splitArrayOfByte(arrayOfByte)
-        print("Amounts of frame received: ", len(listOfByteArray))
-        # byteArray = decode_StringList_To_Byte(listOfByteArray)
-        imageArray = convert_ByteDataArray_To_Mat(listOfByteArray)
-        faceArray = collectStaticData(imageArray)
-        imagePath = createFolder(studentId)
-        storeListOfMatTypeImageToDisk(faceArray, imagePath)
-        clientSocket.sendall(b"DONE\n")
-        print("DONE")
+        if(len(listOfByteArray) == 1):
+            imageArray = convert_ByteDataArray_To_Mat(listOfByteArray)
+            faceArray = imageArray
+            faceArray = collectStaticData(imageArray)
+            # if(len(faceArray) != 1):
+            #     clientSocket.sendall(b"FAILURE\n")
+            #     continue
+            im_data = convertMatFrameTypeToByteFrameType(faceArray[-1])
+            image_data = base64.b64encode(im_data)
+            clientSocket.sendall(b"IMAGE\n")
+            clientSocket.sendall(image_data)
+            clientSocket.sendall(b"\n")
+
+    
+
+def train(clientSocket, clientSocketArrdress, clientSocketPort):
+    studentId = ""
+    arrayOfByte = bytearray()
+    clientSocket.sendall(b"OK\n")
+    signal = readLineFromSocketStream(clientSocket, clientSocketArrdress, clientSocketPort)
+    if(signal == "EXIT"):
+        return
+    studentId = signal
+    print(studentId)
+    arrayOfByte = handle_Image_Send_From_Client(clientSocket, clientSocketArrdress, clientSocketPort)
+    listOfByteArray = splitArrayOfByte(arrayOfByte)
+    print("Amounts of frame received: ", len(listOfByteArray))
+    # byteArray = decode_StringList_To_Byte(listOfByteArray)
+    imageArray = convert_ByteDataArray_To_Mat(listOfByteArray)
+    faceArray = collectStaticData(imageArray)
+    imagePath = createFolder(studentId)
+    storeListOfMatTypeImageToDisk(faceArray, imagePath)
+    clientSocket.sendall(b"DONE\n")
+    print("DONE")
 
 
     # clientSocket.sendall(b"TRAIN_SUCCESS\n")
@@ -288,103 +333,88 @@ def train(clientSocket, clientSocketArrdress, clientSocketPort):
 
 def recognize_And_Response_Result(clientSocket, clientSocketArrdress, clientSocketPort):
     arrayOfByte = bytearray()
-    while(True):
-        clientSocket.sendall(b"OK\n")
-        try:
-            byteReceived = bytearray(clientSocket.recv(1024))
-        except ConnectionResetError:
-            print("Connection reset")
-            return
+    clientSocket.sendall(b"OK\n")
+    arrayOfByte = handle_Image_Send_From_Client(clientSocket, clientSocketArrdress, clientSocketPort)
+    if(len(arrayOfByte) <= 0):
+        print("no frame")
+        return
+    listOfByteArray = splitArrayOfByte(arrayOfByte)
+    print("Amounts of frame received: ", len(listOfByteArray))
+    # byteArray = decode_StringList_To_Byte(listOfByteArray)
+    imageList = convert_ByteDataArray_To_Mat(listOfByteArray)
+    print("Amounts of frame after decode from String to Mat type: ", len(imageList))
 
-        print("Bytes received: ", len(byteReceived))
-
-        if(byteReceived[-1] == 10):
-            byteReceived.pop(-1)
-            signal = byteReceived.decode("utf-8")
-            print(signal)
-            if(signal == "EXIT"):
-                return
-        
-        arrayOfByte = handle_Image_Send_From_Client(clientSocket, clientSocketArrdress, clientSocketPort)
-        listOfByteArray = splitArrayOfByte(arrayOfByte)
-        print("Amounts of frame received: ", len(listOfByteArray))
-        # byteArray = decode_StringList_To_Byte(listOfByteArray)
-        imageList = convert_ByteDataArray_To_Mat(listOfByteArray)
-        print("Amounts of frame after decode from String to Mat type: ", len(imageList))
-
-        #loop to get final frame in framelist
-        for frame in imageList:
-            count = 1
-            # cv2.imshow('img', frame)
-            # if cv2.waitKey(0) & 0xFF == ord('q'):
-            #     cv2.destroyAllWindows()
-                
-        #Recognize Process using harrcascades to detect face and coffe model to recognize face
-        # frame = recognize(frame)
-        ##################################################################
-
-        #Test value without using recognize
-        # recognizeResult = "tu"
-        # name = recognizeResult
-        ##################################################################
-
-        #Recognize Process using deep learning to detect face and coffe model to recognize face
-        try:
-            (name, proba, frame) = ten_image_average(imageList)
-            recognizeResult = name
-        except:
-            frame = None
-        ##################################################################
-        
-
-        #If frame is None. Recognize Process Failure so return Failure signal to client
-        if(frame is None):
-            clientSocket.sendall(b"FAILURE\n")
-            continue
-
-        print(recognizeResult)
-        
-        #Convert image from Mat type to base64 String to send to client 
-        im = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        output = BytesIO()
-        im.save(output, format='JPEG')
-        im_data = output.getvalue()
-        image_data = base64.b64encode(im_data)
-
-
-        print("Bytes send to client: ", len(image_data))
-
-        # if not isinstance(image_data, str):
-        #     # Python 3, decode from bytes to string
-        #     image_data = image_data.decode()
-
-        #Send response to client. With 'Success' signal to notification client recognize process is success
-        #First response process:
-        #Send 'Image' signal to notification client that next resonse is result of recognize process in Image format
-        #Send result Image in Base64 String format
-        #Send 'OK' signal to client to notification end of this response process
-        clientSocket.sendall(b"SUCCESS\n")
-        clientSocket.sendall(b"IMAGE\n")
-        clientSocket.sendall(image_data)
-        clientSocket.sendall(b"\n")
-        clientSocket.sendall(b"DONE\n")
-        ##################################################################
-
-        # print(recognizeResult)
-
-        #Second response process:
-        #Send 'Info' signal to notification client that next resonse is result of recognize process in Text format
-        #Send result Text in String format
-        #Send 'OK' signal to client to notification end of this response process
-        clientSocket.sendall(b"INFO\n")
-        # clientSocket.sendall(recognizeResult.encode())
-        clientSocket.sendall(name.encode())
-        clientSocket.sendall(b"\n")
-        clientSocket.sendall(b"DONE\n")
-
+    #loop to get final frame in framelist
+    for frame in imageList:
+        count = 1
+        # cv2.imshow('img', frame)
+        # if cv2.waitKey(0) & 0xFF == ord('q'):
+        #     cv2.destroyAllWindows()
             
+    #Recognize Process using harrcascades to detect face and coffe model to recognize face
+    # frame = recognize(frame)
+    ##################################################################
+    #Test value without using recognize
+    # recognizeResult = "tu"
+    # name = recognizeResult
+    ##################################################################
+
+    #Recognize Process using deep learning to detect face and coffe model to recognize face
+    try:
+        (name, proba, frame) = ten_image_average(imageList)
+        print(name)
+        recognizeResult = name
+    except:
+        print("Exception")
+        frame = None
+    ##################################################################
     
 
+    #If frame is None. Recognize Process Failure so return Failure signal to client
+    if(frame is None):
+        clientSocket.sendall(b"FAILURE\n")
+        return
+
+    print(recognizeResult)
+    
+    #Convert image from Mat type to base64 String to send to client 
+    im = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    output = BytesIO()
+    im.save(output, format='JPEG')
+    im_data = output.getvalue()
+    image_data = base64.b64encode(im_data)
+
+
+    print("Bytes send to client: ", len(image_data))
+
+    # if not isinstance(image_data, str):
+    #     # Python 3, decode from bytes to string
+    #     image_data = image_data.decode()
+
+    #Send response to client. With 'Success' signal to notification client recognize process is success
+    #First response process:
+    #Send 'Image' signal to notification client that next resonse is result of recognize process in Image format
+    #Send result Image in Base64 String format
+    #Send 'OK' signal to client to notification end of this response process
+    clientSocket.sendall(b"SUCCESS\n")
+    clientSocket.sendall(b"IMAGE\n")
+    clientSocket.sendall(image_data)
+    clientSocket.sendall(b"\n")
+    clientSocket.sendall(b"DONE\n")
+    ##################################################################
+
+    # print(recognizeResult)
+
+    #Second response process:
+    #Send 'Info' signal to notification client that next resonse is result of recognize process in Text format
+    #Send result Text in String format
+    #Send 'OK' signal to client to notification end of this response process
+    clientSocket.sendall(b"INFO\n")
+    # clientSocket.sendall(recognizeResult.encode())
+    clientSocket.sendall(name.encode())
+    clientSocket.sendall(b"\n")
+    clientSocket.sendall(b"DONE\n")
+    
 
 def handlle_client(clientSocket, clientSocketArrdress, clientSocketPort):
     print ("Connection from : " + clientSocketArrdress + ":" + str(clientSocketPort))
@@ -393,27 +423,29 @@ def handlle_client(clientSocket, clientSocketArrdress, clientSocketPort):
     # f = open(system_path, "wb")
     while True:
         print("begin")
-        while True:
-            try:
-                byteReceived = bytearray(clientSocket.recv(1024))
-            except ConnectionResetError:
-                cv2.destroyAllWindows()
-                print("Connection reset")
-                return
+        try:
+            byteReceived = bytearray(clientSocket.recv(1024))
+        except ConnectionResetError:
+            cv2.destroyAllWindows()
+            print("Connection reset")
+            return
 
-            print("Bytes received: ", len(byteReceived))
+        print("Bytes received: ", len(byteReceived))
 
-            if(byteReceived[-1] == 10):
-                byteReceived.pop(-1)
-                clientSignal = byteReceived.decode("utf-8")
-                print(clientSignal)
-                if(clientSignal == "TRAIN"):
-                    print("train")
-                    train(clientSocket, clientSocketArrdress, clientSocketPort)
-                if(clientSignal == "RECOGNIZE"):
-                    print("recognize")
-                    recognize_And_Response_Result(clientSocket, clientSocketArrdress, clientSocketPort)
-                    
+        if(byteReceived[-1] == 10):
+            byteReceived.pop(-1)
+            clientSignal = byteReceived.decode("utf-8")
+            print(clientSignal)
+            if(clientSignal == "TRAIN"):
+                print("train")
+                train(clientSocket, clientSocketArrdress, clientSocketPort)
+            if(clientSignal == "RECOGNIZE"):
+                print("recognize")
+                recognize_And_Response_Result(clientSocket, clientSocketArrdress, clientSocketPort)
+            if(clientSignal == "DETECT"):
+                print("detect")
+                realTimeFaceDetect(clientSocket, clientSocketArrdress, clientSocketPort)
+                
         # break
     clientSocket.close()
     return
